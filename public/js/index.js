24e3f60e4662958104c35b6f7cc3cc475115619e
booldook <booldook@gmail.com>
2020-10-21 11:20:43 +0900

    swiper 적용 전

--- public/js/index.js
+++ public/js/index.js
@@ -100,6 +100,13 @@ function onNaviLoad(r) {
 	$(".navi-wrap > .navi").mouseenter(onEnter);
 	$(".navi-wrap > .navi").mouseleave(onLeave);
 	$(".sub-slide .color").find("span").click(onColorClick);
+	$(".sub-slide .wrap").swipe({
+		swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
+			if(direction == 'left') $(".sub-slide .bt-next").trigger("click");
+			if(direction == 'right') $(".sub-slide .bt-prev").trigger("click");
+		},
+		threshold: 30
+	});
 	$(".sub-slide .bt-prev").click(onSubPrevClick);
 	$(".sub-slide .bt-next").click(onSubNextClick);
 }
@@ -111,6 +118,198 @@ function onNaviLoad(r) {
 			<div class="title">1. HOME DEFAULT</div>
 			<div class="cont-img"><img src="../img/default.jpg" alt="그림" class="w-100"></div>
 		</div>
+	</div>
+</div>
+
+
+
+<div class="sub-slide">
+	<div class="stage">
+		<div class="wrap">
+			<div class="slide">
+				<div class="img-wrap">
+					<div class="img-case active">
+						<img src="../img/ss-01-blue-01.jpg" class="w-100">
+						<img src="../img/ss-01-blue-02.jpg" class="w-100">
+					</div>
+					<div class="img-case">
+						<img src="../img/ss-01-black-01.jpg" class="w-100">
+						<img src="../img/ss-01-black-02.jpg" class="w-100">
+					</div>
+					<div class="img-case">
+						<img src="../img/ss-01-yellow-01.jpg" class="w-100">
+						<img src="../img/ss-01-yellow-02.jpg" class="w-100">
+					</div>
+					<div class="bt bt-quick">
+						<i class="fa fa-shopping-cart"></i> QUICK SHOP
+					</div>
+					<div class="bt bt-icon bt-heart">
+						<div class="popper">
+							Login to use Wishlist <i class="fa fa-caret-right"></i>
+						</div>
+						<i class="far fa-heart"></i>
+					</div>
+					<div class="bt bt-icon bt-sync">
+						<div class="popper">
+							Compare <i class="fa fa-caret-right"></i>
+						</div>
+						<i class="fa fa-sync"></i>
+					</div>
+					<div class="bt bt-icon bt-search">
+						<div class="popper">
+							Quick View <i class="fa fa-caret-right"></i>
+						</div>
+						<i class="fa fa-search-plus"></i>
+					</div>
+				</div>
+				<div class="color">
+					<span class="blue">●</span>
+					<span class="black">●</span>
+					<span class="yellow">●</span>
+				</div>
+				<div class="title">Yus condntum sapien</div>
+				<div class="brand">BASEL</div>
+				<div class="price">$592.00</div>
+			</div>
+			<div class="slide">
+				<div class="img-wrap">
+					<div class="img-case active">
+						<img src="../img/ss-02-blue-01.jpg" class="w-100">
+					</div>
+					<div class="img-case">
+						<img src="../img/ss-02-red-01.jpg" class="w-100">
+						<img src="../img/ss-02-red-02.jpg" class="w-100">
+					</div>
+					<div class="img-case">
+						<img src="../img/ss-02-yellow-01.jpg" class="w-100">
+					</div>
+					<div class="bt bt-quick">
+						<i class="fa fa-shopping-cart"></i> QUICK SHOP
+					</div>
+					<div class="bt bt-icon bt-heart">
+						<div class="popper">
+							Login to use Wishlist <i class="fa fa-caret-right"></i>
+						</div>
+						<i class="far fa-heart"></i>
+					</div>
+					<div class="bt bt-icon bt-sync">
+						<div class="popper">
+							Compare <i class="fa fa-caret-right"></i>
+						</div>
+						<i class="fa fa-sync"></i>
+					</div>
+					<div class="bt bt-icon bt-search">
+						<div class="popper">
+							Quick View <i class="fa fa-caret-right"></i>
+						</div>
+						<i class="fa fa-search-plus"></i>
+					</div>
+				</div>
+				<div class="color">
+					<span class="blue">●</span>
+					<span class="red">●</span>
+					<span class="yellow">●</span>
+				</div>
+				<div class="title">Yom orki lacinia</div>
+				<div class="brand">BASEL</div>
+				<div class="price">
+					<span class="price-def">$799.00</span>
+					<span class="price-sale">$592.00</span>
+				</div>
+			</div>
+			<div class="slide">
+				<div class="img-wrap">
+					<div class="img-case active">
+						<img src="../img/ss-01-yellow-01.jpg" class="w-100">
+						<img src="../img/ss-01-yellow-02.jpg" class="w-100">
+					</div>
+					<div class="img-case">
+						<img src="../img/ss-01-black-01.jpg" class="w-100">
+						<img src="../img/ss-01-black-02.jpg" class="w-100">
+					</div>
+					<div class="img-case">
+						<img src="../img/ss-01-blue-01.jpg" class="w-100">
+						<img src="../img/ss-01-blue-02.jpg" class="w-100">
+					</div>
+					<div class="bt bt-quick">
+						<i class="fa fa-shopping-cart"></i> QUICK SHOP
+					</div>
+					<div class="bt bt-icon bt-heart">
+						<div class="popper">
+							Login to use Wishlist <i class="fa fa-caret-right"></i>
+						</div>
+						<i class="far fa-heart"></i>
+					</div>
+					<div class="bt bt-icon bt-sync">
+						<div class="popper">
+							Compare <i class="fa fa-caret-right"></i>
+						</div>
+						<i class="fa fa-sync"></i>
+					</div>
+					<div class="bt bt-icon bt-search">
+						<div class="popper">
+							Quick View <i class="fa fa-caret-right"></i>
+						</div>
+						<i class="fa fa-search-plus"></i>
+					</div>
+				</div>
+				<div class="color">
+					<span class="yellow">●</span>
+					<span class="black">●</span>
+					<span class="blue">●</span>
+				</div>
+				<div class="title">Yus condntum sapien</div>
+				<div class="brand">BASEL</div>
+				<div class="price">$592.00</div>
+			</div>
+			<div class="slide">
+				<div class="img-wrap">
+					<div class="img-case active">
+						<img src="../img/ss-01-blue-01.jpg" class="w-100">
+						<img src="../img/ss-01-blue-02.jpg" class="w-100">
+					</div>
+					<div class="img-case">
+						<img src="../img/ss-01-black-01.jpg" class="w-100">
+						<img src="../img/ss-01-black-02.jpg" class="w-100">
+					</div>
+					<div class="img-case">
+						<img src="../img/ss-01-yellow-01.jpg" class="w-100">
+						<img src="../img/ss-01-yellow-02.jpg" class="w-100">
+					</div>
+					<div class="bt bt-quick">
+						<i class="fa fa-shopping-cart"></i> QUICK SHOP
+					</div>
+					<div class="bt bt-icon bt-heart">
+						<div class="popper">
+							Login to use Wishlist <i class="fa fa-caret-right"></i>
+						</div>
+						<i class="far fa-heart"></i>
+					</div>
+					<div class="bt bt-icon bt-sync">
+						<div class="popper">
+							Compare <i class="fa fa-caret-right"></i>
+						</div>
+						<i class="fa fa-sync"></i>
+					</div>
+					<div class="bt bt-icon bt-search">
+						<div class="popper">
+							Quick View <i class="fa fa-caret-right"></i>
+						</div>
+						<i class="fa fa-search-plus"></i>
+					</div>
+				</div>
+				<div class="color">
+					<span class="blue">●</span>
+					<span class="black">●</span>
+					<span class="yellow">●</span>
+				</div>
+				<div class="title">Yus condntum sapien</div>
+				<div class="brand">BASEL</div>
+				<div class="price">$592.00</div>
+			</div>
+		</div>
+		<div class="bt-pager bt-prev">〈</div>
+		<div class="bt-pager bt-next">〉</div>
 	</div>
 </div>
 */
